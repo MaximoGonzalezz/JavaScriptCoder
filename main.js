a35ffd5057eb44e3ad7daeea95c59d40ed17a188
MaximoGonzalezz <maxug2@gmail.com>
2023-11-22 20:23:45 -0300

    ultimo commit

--- main.js
+++ main.js
@@ -1,91 +1,76 @@
-const shopContenedor = document.getElementById("shopContenedor");
-const verCarrito = document.getElementById("verCarrito");
-const modalContainer = document.getElementById("barraContainer");
 
+document.addEventListener('DOMContentLoaded', function () {
+  const listaCarrito = document.getElementById('cart-items');
+  const totalElemento = document.getElementById('total');
+  const botonesAgregarAlCarrito = document.querySelectorAll('.add-to-cart');
 
-const productos = [
-  {
-    id: 1,
-    nombre: "sorrentinos",
-    precio: 1500,
-    img: "",
-  },
+  
+  const productos = [
+    { id: 3, price: 1500.00, name: 'Sorrentinos', image: '/img/sorrentienda.jpg' },
+    { id: 4, price: 1800.00, name: 'Ravioles', image: '/img/ravioltienda.jpg' },
+    { id: 5, price: 1500.00, name: 'Ñoquis rellenos', image: '/img/ñoquipanceta.jpg' },
+    { id: 6, price: 1300.00, name: 'Ñoquis', image: '/img/ñoqui.jpg' },
+  ];  
+  
+  cargarCarrito();
 
-  {
-    id: 1,
-    nombre: "ravioles",
-    precio: 1500,
-    img: "https://stock.adobe.com/es/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bcontent_type%3Aaudio%5D=0&filters%5Binclude_stock_enterprise%5D=0&filters%5Bis_editorial%5D=0&filters%5Bfree_collection%5D=0&filters%5Bcontent_type%3Aimage%5D=1&k=ravioles&order=relevance&safe_search=1&search_page=1&search_type=usertyped&acp=&aco=ravioles&get_facets=0&asset_id=610132820 ",
-
-  },
-  {
-    id: 1,
-    nombre: "ñoquis rellenos",
-    precio: 1300,
-    img: "https://stock.adobe.com/es/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bcontent_type%3Aimage%5D=1&order=relevance&safe_search=1&limit=100&search_page=1&search_type=usertyped&acp=&aco=%C3%B1oquis+rellenos&k=%C3%B1oquis+rellenos&get_facets=0&asset_id=495315406",
-  },
-  {
-    id: 1,
-    nombre: "ñoquis",
-    precio: 1000,
-    img: "https://stock.adobe.com/es/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bcontent_type%3Aimage%5D=1&order=relevance&safe_search=1&limit=100&search_page=1&search_type=usertyped&acp=&aco=%C3%B1oquis&k=%C3%B1oquis&get_facets=0&asset_id=330259977",
-  },
-];
-
-let carrito = [];
-
-productos.forEach((producto) => {
-  let content = document.createElement("div");
-  content.className = "card";
-  content.innerHTML = `
-  <img src="${producto.img}">
-  <h3> ${producto.nombre}  </h3>
-  <p class="precio"> ${producto.precio} $ </p>
-  `;
-
-  shopContenedor.append(content);
-
-  let comprar = document.createElement("button")
-  comprar.innerText = "comprar";
-  comprar.className = "comprar";
-
-  content.append(comprar);
-
-  comprar.addEventListener("click", () => {
-    carrito.push({
-      id: producto.id,
-      img: producto.img,
-      nombre: producto.nombre,
-      precio: producto.precio,
-
-    });
-  })
-
+  botonesAgregarAlCarrito.forEach(boton => {
+    boton.addEventListener('click', agregarAlCarrito);
   });
 
-  verCarrito.addEventListener("click", () => {
+  function agregarAlCarrito(evento) {
+    const productoId = parseInt(evento.target.closest('li').getAttribute('data-id'));
+    const productoSeleccionado = productos.find(producto => producto.id === productoId);
 
-    const modalHeader = document.createElement("div");
-    modalHeader.className = "barra-header"
-    modalHeader.innerHTML = ` 
-    <h2 class= "modal-header-titulo"> Carrito. </h2>` ;
+    if (productoSeleccionado) {
+      agregarProductoAlCarrito(productoSeleccionado);
+      actualizarTotal();
+      guardarCarrito();
+    }
+  }
 
-    barraContainer.append(modalHeader);
-    const modalButton = document.createElement("h2");
-    modalButton.innerText = "x";
-    modalButton.className = "barra-header-button";
-    modalHeader.append(modalButton);
+  function agregarProductoAlCarrito(producto) {
+    const elementoCarrito = document.createElement('li');
+    elementoCarrito.innerHTML = `
+          <div class="product-item">
+              <img src="${producto.image}" alt="${producto.name}">
+              <p>${producto.name} - $${producto.price.toFixed(2)}</p>
+              <button class="remove-from-cart">Eliminar</button>
+          </div>`;
+    elementoCarrito.setAttribute('data-id', producto.id);
+    elementoCarrito.setAttribute('data-price', producto.price);
 
-    carrito.forEach((producto) => {
-      let carritoContent = document.createElement("div")
-    carritoContent.className = "modal-content"
-    carritoContent.innerHTML = `
-    <h2> ${producto.nombre}</h2>
-    <p> ${producto.precio} $ </p>
-    `;
+    listaCarrito.appendChild(elementoCarrito);
+  }
 
-      modalContainer.append(carritoContent);
+  listaCarrito.addEventListener('click', function (evento) {
+    if (evento.target.classList.contains('remove-from-cart')) {
+      const elementoEliminar = evento.target.closest('li');
+      elementoEliminar.parentNode.removeChild(elementoEliminar);
+      actualizarTotal();
+      guardarCarrito();
+    }
+  });
 
-    });
-  })
+  function actualizarTotal() {
+    const preciosElementosCarrito = Array.from(listaCarrito.children).map(item => parseFloat(item.getAttribute('data-price')));
+    const total = preciosElementosCarrito.reduce((sum, precio) => sum + precio, 0).toFixed(2);
+    totalElemento.textContent = `Total: $${ total }`;
+    localStorage.setItem('totalCarrito', total);
+  }
 
+  function cargarCarrito() {
+    const carritoGuardado = localStorage.getItem('carrito');
+    const totalGuardado = localStorage.getItem('totalCarrito');
+
+    if (carritoGuardado) {
+      listaCarrito.innerHTML = carritoGuardado;
+      totalElemento.textContent = `Total: $${ totalGuardado || '0.00' }`;
+    }
+  }
+
+  function guardarCarrito() {
+    localStorage.setItem('carrito', listaCarrito.innerHTML);
+    actualizarTotal();
+  }
+});
